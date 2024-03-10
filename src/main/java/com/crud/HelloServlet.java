2865b0dd930995f92f489ff731a687d0ad3be004
Youssafouhba <youssefouhba@gmail.com>
2024-03-10 20:09:09 +0000

    first commit

--- src/main/java/com/crud/HelloServlet.java
+++ src/main/java/com/crud/HelloServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/")
 public class HelloServlet extends HttpServlet {
     private String message;
 
@@ -15,7 +15,7 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
+        this.getServletContext().getRequestDispatcher("/ListClients.xhtml").forward(request,response);
     }
 
     public void destroy() {
