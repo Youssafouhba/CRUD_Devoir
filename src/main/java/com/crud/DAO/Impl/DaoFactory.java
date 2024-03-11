3430c01f95314765420f940bbde6bc2f9a87988e
Ouhba Youssef <146470979+Youssafouhba@users.noreply.github.com>
2024-03-11 13:11:20 +0000

    Update DaoFactory.java

--- src/main/java/com/crud/DAO/Impl/DaoFactory.java
+++ src/main/java/com/crud/DAO/Impl/DaoFactory.java
@@ -21,10 +21,10 @@ public class DaoFactory {
         } catch (ClassNotFoundException e) {
 
         }
-        return new DaoFactory("jdbc:mysql://mysql-2c3b7474-youssefouhba-1406.a.aivencloud.com:23229/hi", "avnadmin", "AVNS_hdj58afUSowWP3eu2GF");
+        return new DaoFactory("jdbc:mysql://localhost:3306/hi", "root", "");
     }
     public Connection getConnection() throws SQLException {
-        Connection con = DriverManager.getConnection("jdbc:mysql://mysql-2c3b7474-youssefouhba-1406.a.aivencloud.com:23229/hi?user=avnadmin&password=AVNS_hdj58afUSowWP3eu2GF");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hi?user=root");
         return con;
     }
 }
